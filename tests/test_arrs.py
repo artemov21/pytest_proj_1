d03f2653cb462535c8d3db3e01449da8c3bf1546
artemov21 <artemartemov2134@mail.ru>
2023-03-23 06:54:20 +0300

    fix bug

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,9 +5,10 @@ def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], -1, "test") == "test"
 
-    print('fix bug')
-
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1, 2, 3],-1) == [3]
+    assert arrs.my_slice([1], -2) == [1]
